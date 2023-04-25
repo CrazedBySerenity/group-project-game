bf458a6b04d0c8737e77cb9809545658969765fc
CrazedBySerenity <villeisakd@gmail.com>
2023-04-25 11:29:10 +0200

    Added input to move the player up and down, added an infinitely scrolling background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+  const [playerPosition, setPlayerPosition] = useState(250);
+  const [playerSpeed, setPlayerSpeed] = useState(10);
+  const [tileOnePos, setTileOnePos] = useState(0);
+  const [tileTwoPos, setTileTwoPos] = useState(2000);
+
+  const validUpKeyCodes = [38, 87];
+  const validDownKeyCodes = [40, 83];
+
+  const playerSize = 40;
+  const gameAreaSize = 500;
+  const bgScrollSpeed = 5;
+
+  const gameAreaStyle = {
+    backgroundColor: 'black',
+    width: `${gameAreaSize}px`,
+    height: `${gameAreaSize}px`,
+  }
+
+  const playerStyle = {
+    position: `absolute`,
+    backgroundColor: 'red',
+    width: `${playerSize}px`,
+    height: `${playerSize}px`,
+    top: `${playerPosition}px`,
+  };
+
+  const tileOneStyle = {
+    left: `${tileOnePos}px`,
+  };
+
+  const tileTwoStyle = {
+    left: `${tileTwoPos}px`,
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) =>{
+      console.log(e.keyCode);
+
+      if(validUpKeyCodes.includes(e.keyCode)){
+        setPlayerPosition(playerPosition => playerPosition - playerSpeed)
+      }
+      if(validDownKeyCodes.includes(e.keyCode)){
+        setPlayerPosition(playerPosition => playerPosition + playerSpeed)
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown) 
+  }, [])
+
+  useEffect(() => {
+    let timeId;
+    timeId = setInterval(() => {
+      setTileOnePos((tileOnePos) => tileOnePos - bgScrollSpeed);
+      setTileTwoPos((tileTwoPos) => tileTwoPos - bgScrollSpeed);
+
+      if(tileOnePos < -2000){
+        setTileOnePos(2000);
+      }
+      if(tileTwoPos < -2000){
+        setTileTwoPos(2000);
+      }
+      
+    }, 24);
+
+    return(() => {
+      clearInterval(timeId);
+    });
+  }, [tileOnePos, tileTwoPos])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={gameAreaStyle} className="game-area">
+        <div className="background__container">
+          <div style={tileOneStyle} className="background__tile"></div>
+          <div style={tileTwoStyle} className="background__tile"></div>
+        </div>
+        <div>
+          
+        </div>
+        <div style={playerStyle} className="player"></div>
+      </div>
+
     </div>
   );
 }
