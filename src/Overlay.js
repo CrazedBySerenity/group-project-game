f49be1c496469d4342e1cd8b67e7286a3d433d67
Oreazuz <Omaddams@hotmail.com>
2023-05-12 13:54:31 +0200

    merge issues

--- src/Overlay.js
+++ src/Overlay.js
@@ -13,37 +13,36 @@ const Overlay = (props) => {
   } else if (props.userLogin) {
     return <Login></Login>;
   } else if (props.gameOver) {
-    if (gameOver) {
-      return (
-        <div className="wh-100 flex-center">
-          <div className="game-over__container flex-center">
-            <p className="game-over__text">Game over!</p>
-            <div>
-              <p className="game-over__text">Final Score</p>
-              <p className="game-over__text">{score}</p>
-            </div>
-            <p className="game-over__text">Press ENTER to Restart...</p>
+    return (
+      <div className="wh-100 flex-center">
+        <div className="game-over__container flex-center">
+          <p className="game-over__text">Game over!</p>
+          <div>
+            <p className="game-over__text">Final Score</p>
+            <p className="game-over__text">{props.score}</p>
           </div>
+          <p className="game-over__text">Press ENTER to Restart...</p>
         </div>
-      );
-    } else if (!gameStarted) {
-      return (
-        <div className="wh-100 flex-center">
-          <div className="game-start__container flex-center">
-            <p className="game-over__text">Shoot down all the asteroids!</p>
-            <div style={{ display: "flex" }}>
-              <p className="game-over__text">
-                Use W/S or Arrow up/ Arrow down to move up and down
-              </p>
-              <p className="game-over__text">Use SPACEBAR to shoot</p>
-            </div>
-            <p className="game-over__text">Move up or down to start!</p>
+      </div>
+    );
+  } else if (!props.gameStarted) {
+    return (
+      <div className="wh-100 flex-center">
+        <div className="game-start__container flex-center">
+          <p className="game-over__text">Shoot down all the asteroids!</p>
+          <div style={{ display: "flex" }}>
+            <p className="game-over__text">
+              Use W/S or Arrow up/ Arrow down to move up and down
+            </p>
+            <p className="game-over__text">Use SPACEBAR to shoot</p>
           </div>
+          <p className="game-over__text">Move up or down to start!</p>
         </div>
-      );
-    }
-
-    return <>{/* <ScoreDisplay score={props.score} /> */}</>;
+      </div>
+    );
   }
+
+  return <>{/* <ScoreDisplay score={props.score} /> */}</>;
 };
+
 export default Overlay;
